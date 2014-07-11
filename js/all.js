665ce855f81bf023ddad4ceba5643a842b2ae859
shen2 <zhenyupku@gmail.com>
2014-07-12 02:58:14 +0800

    初始化过程重构，修改样式

--- js/all.js
+++ js/all.js
@@ -1,84 +1,87 @@
 (function(window, $){
-	var NAME = function(){
-		this.defaultIndex = 'index.md';
+
+var NAME = function(){
+	this.defaultRoot = 'README.md';
+	this.defaultIndex = 'index.md';
+	
+	this.parser = {};
+	this.parser['md'] = this.parser['markdown'] = function(rsp){
+		this.html(marked(rsp, true));
+		document.title = this.find('h1').text();
 		
-		this.parser = {};
-		this.parser['md'] = this.parser['markdown'] = function(rsp){
-			this.html(marked(rsp, true));
-			document.title = this.find('h1').text();
-			
-			return this;
-		};
-		
-		this.parser['txt'] = $.fn.text;
-		
-		this.parser['html'] = $.fn.html;
+		return this;
 	};
 	
-	NAME.prototype.navigate = function(pathname){
-		var self = this;
+	this.parser['txt'] = $.fn.text;
+	this.parser['html'] = $.fn.html;
+};
+
+NAME.prototype.navigate = function(pathname){
+	var self = this;
+	var url = pathname;
+	
+	if (pathname === ''){
+		url += this.defaultRoot;
+	}
+	else if (pathname.charAt(pathname.length - 1) === '/'){
+		url += this.defaultIndex;
+	}
+	
+	$.ajax({
+		url  : url,
+	    type: 'GET',
+		complete: function(){},
+		success: function(rsp, result, xhr){
+			var main = $('main');
+			
+			var matches = url.match(/\.([a-z0-9]+)$/i);
+			
+			var parser = matches && self.parser[matches[1]] ? self.parser[matches[1]] : self.parser['html'];
+			
+			parser.call(main, rsp);
+		},
+		error : function(){
+			var main = $('main');
+			main.html('<h1>404 Not Found</h1>');
+		}
+	});
+};
+
+NAME.prototype.autoload = function (list, ext){
+	list.each(function(){
+		var el = $(this);
 		$.ajax({
-			url  : pathname + (pathname.charAt(pathname.length - 1) === '/' ? this.defaultIndex : ''),
+			url  : this.id +'.' + ext,
 		    type: 'GET',
 			complete: function(){},
 			success: function(rsp, result, xhr){
-				var main = $('main');
-				
-				var matches = pathname.match(/\.([a-z0-9]+)$/i);
-				
-				var parser = matches && self.parser[matches[1]] ? self.parser[matches[1]] : self.parser['html'];
-				
-				parser.call(main, rsp);
-			},
-			error : function(){
-				var main = $('main');
-				main.html('<h1>404 Not Found</h1>');
+				instance.parser[ext].call(el, rsp);
 			}
-		});
-	};
-	
-	window.NAME = NAME;
-
-	var instance = new NAME(); 
-
-	$(function(){
-		
-		var loadHash = function(e){
-			if (window.location.hash === ''){
-				instance.navigate('index.md');
-				return false;
-			}
-			
-			var matches = window.location.hash.match(/^#!(.+)/i);
-			if (matches){
-				instance.navigate(matches[1]);
-				return false;
-			}
-		};
-		
-		if (window.location.pathname){
-			
-		}
-		else{
-			
-		}
-		
-		$(window).on('hashchange', loadHash);
-		
-		loadHash();
-		
-		
-		$('.markdown-partial').each(function(){
-			var el = $(this);
-			$.ajax({
-				url  : '/' + $(el).attr('id') +'.md',//,
-			    type: 'GET',
-				complete: function(){},
-				success: function(rsp, result, xhr){
-					instance.parser['md'].call(el, rsp);
-				}
-			});
 		});
 	});
+};
+
+NAME.prototype.dispatch = function(){
+	var self = this;
+	function router(e){
+		var hash = window.location.hash;
+		if (hash === ''){
+			self.navigate('');
+			return false;
+		}
+		
+		var matches = hash.match(/^#!(.*)/i);
+		if (matches){
+			self.navigate(matches[1]);
+			return false;
+		}
+	}
+	
+	$(window).on('hashchange', router);
+	
+	router();
+};
+
+window.NAME = NAME;
 
 })(window, jQuery);
--- nav.md
+++ nav.md
@@ -1,4 +1,11 @@
-  * [首页](#!)
-  * [获取活动](#!events/get.md)
+[图虫API文档](#!)
+
+### 活动
+ * [获取活动](#!events/get.md)
+
+### 标签
   * [获取标签](#!tag/get.md)
+  * [获取标签分类](#!tag/categories.md)
+
+### 站点
   * [获取站点](#!site/get.md)
